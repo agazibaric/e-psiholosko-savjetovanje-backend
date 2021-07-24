751ca0bcc5f33f7bb828d82f98686fc2e1be1ea9
agazibaric <ante.gazibaric@gmail.com>
2021-07-24 15:58:52 +0200

    Question model

--- src/main/java/com/epsih/model/meeting/Question.java
+++ src/main/java/com/epsih/model/meeting/Question.java
@@ -1,17 +1,16 @@
 package com.epsih.model.meeting;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+import com.epsih.model.service.BusinessService;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "question")
@@ -19,15 +18,35 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Builder
-public class Question {
+public class   Question {
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    @Column(name = "pk_question")
    private Long id;
 
+   @Column(length = 3000)
    @NotNull
-   private String question;
+   private String content;
 
+   @Column
+   @NotNull
+   private Boolean isMultiSelect;
+
+   @Column
+   @NotNull
+   private Boolean isMandatory;
+
+   @OneToOne
+   @JoinColumn(name = "fk_answer_options", referencedColumnName = "pk_answer_options", nullable = true)
+   private AnswerOptions answerOptions;
+
+   @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+   @JsonIgnore
+   private List<Answer> answers;
+
+   @ManyToMany(fetch = FetchType.LAZY, mappedBy = "questions", cascade = CascadeType.MERGE)
+   @JsonIgnore
+   private List<BusinessService> services;
 
 }
